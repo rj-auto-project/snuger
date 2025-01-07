87e795a80472de0677783470cca780131de362ff
rj-auto-project <rjdb707@gmail.com>
2025-01-08 01:38:31 +0530

    create user api 0.0.0

--- index.js
+++ index.js
@@ -1,41 +1,3 @@
-// const credentials = require('./credentials');
-// const express = require("express");
-// const multer = require("multer");
-// const path = require("path");
-// const mongoose = require("mongoose");
-// const userRoute = require("./routes/user");
-
-// const { Storage } = require("@google-cloud/storage");
-// const storage = new Storage({
-//   projectId: process.env.GOOGLE_CLOUD_PROJECT_ID,
-//   credentials:credentials,
-// });
-// const bucketName = "snuger";
-
-// const app = express();
-
-// const upload = multer({
-//   storage: multer.memoryStorage(),
-// });
-
-// const mongoURI = process.env.MONGOURI;
-// mongoose.connect(mongoURI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-// mongoose.connection.on("connected", () => console.log("Connected to MongoDB"));
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-
-// // register user route
-// app.use("/user", userRoute(upload, storage, bucketName));
-
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-
 import fastify from "fastify";
 import { connectDB } from "./src/config/connect.js";
 import env from "./src/config/env.js";
@@ -46,17 +8,15 @@ import { registerRoutes } from "./src/routes/index.js";
 
 const app = fastify();
 
-// Plugins
 app.register(rateLimitPlugin);
 app.register(fastifyCors, {
   origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
 });
-//Error handler
+
 app.setErrorHandler(errorHandler);
-// Routes
+
 app.register(registerRoutes);
-// Start Server
 
 app.get("/", async (request, reply) => {
   return { message: "Hello from Snuger 😎" };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,11 @@
 import { createUser, getUserProfile } from "../controllers/user.controller.js";
+import multipart from "@fastify/multipart";
 
 export const userRoutes = async (fastify, options) => {
-  fastify.post("/", createUser);
+  fastify.register(multipart, {
+    limits: { fileSize: 10 * 1024 * 1024 }, // 10 MB
+  });
+  fastify.post("/create", createUser);
   fastify.get("/:id", getUserProfile);
+
 };
