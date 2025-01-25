0e69794f29581379975175fa87561decec93d412
rj-auto-project <rjdb707@gmail.com>
2025-01-25 21:12:25 +0530

    minor error mix in userCOunt api

--- src/routes/userCount.route.js
+++ src/routes/userCount.route.js
@@ -2,5 +2,5 @@ import fastifyMultipart from "@fastify/multipart";
 import { updateUserCount } from "../controllers/userCount.controller.js";
 
 export const userCountRoutes = async (fastify) => {
-  fastify.put("/increment",updateUserCount)
+  fastify.post("/increment",updateUserCount)
 };
