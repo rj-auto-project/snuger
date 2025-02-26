e9291681a8ad20a6090fca218a4a479cee42f766
rj-auto-project <rjdb707@gmail.com>
2025-02-26 20:38:51 +0530

    redis testing

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.get("/", async (request, reply) => {
 
 // Redis client
 const redisClient = new Redis({
-  host: ' 10.113.121.147',
+  host: '10.113.121.147',
   port: 6379,
   maxRetriesPerRequest: 3,
   retryStrategy(times) {
