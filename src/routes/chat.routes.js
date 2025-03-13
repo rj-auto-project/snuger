4c32bf9f818180166c3fa95848db8109dce7e778
rj-auto-project <rjdb707@gmail.com>
2025-03-13 13:45:17 +0530

    clean chatapi

--- src/routes/chat.routes.js
+++ src/routes/chat.routes.js
@@ -1,43 +1,43 @@
-import { getChats, getOrCreateChat } from "../controllers/chat.controller.js";
-import { getMessages } from "../controllers/message.controller.js";
+// import { getChats, getOrCreateChat } from "../controllers/chat.controller.js";
+// import { getMessages } from "../controllers/message.controller.js";
 
-export const chatRoutes = async (fastify) => {
-  fastify.get(
-    "/",
-    {
-      schema: {
-        querystring: {
-          type: "object",
-          properties: {
-            page: { type: "integer", minimum: 1, default: 1 },
-            limit: { type: "integer", minimum: 1, maximum: 100, default: 20 },
-          },
-          additionalProperties: false,
-        },
-      },
-    },
-    getChats
-  );
+// export const chatRoutes = async (fastify) => {
+//   fastify.get(
+//     "/",
+//     {
+//       schema: {
+//         querystring: {
+//           type: "object",
+//           properties: {
+//             page: { type: "integer", minimum: 1, default: 1 },
+//             limit: { type: "integer", minimum: 1, maximum: 100, default: 20 },
+//           },
+//           additionalProperties: false,
+//         },
+//       },
+//     },
+//     getChats
+//   );
 
-  fastify.get(
-    "/:participantId",
-    getOrCreateChat
-  );
+//   fastify.get(
+//     "/:participantId",
+//     getOrCreateChat
+//   );
 
-  fastify.get(
-    "/:chatId/messages",
-    {
-      schema: {
-        querystring: {
-          type: "object",
-          properties: {
-            page: { type: "integer", minimum: 1, default: 1 },
-            limit: { type: "integer", minimum: 1, maximum: 100, default: 20 },
-          },
-          additionalProperties: false,
-        },
-      },
-    },
-    getMessages
-  );
-};
+//   fastify.get(
+//     "/:chatId/messages",
+//     {
+//       schema: {
+//         querystring: {
+//           type: "object",
+//           properties: {
+//             page: { type: "integer", minimum: 1, default: 1 },
+//             limit: { type: "integer", minimum: 1, maximum: 100, default: 20 },
+//           },
+//           additionalProperties: false,
+//         },
+//       },
+//     },
+//     getMessages
+//   );
+// };
